0a79b19c1943aad87ec12ee32b8203e94efc5857
yoezequiel <ezequielcasiano15@gmail.com>
2025-12-30 17:33:56 -0300

    Actualizar la URL del sitio en la configuración de Astro para apuntar a la versión de producción.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
-    site: "http://localhost:4321", // Cambiar a la URL de producción cuando se despliegue
+    site: "https://POS-yoezequiel.vercel.app", // Cambiar a la URL de producción cuando se despliegue
     integrations: [react()],
     server: {
         port: 4321,
