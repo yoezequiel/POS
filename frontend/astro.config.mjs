c555407cc7bd2110c1cfcc9b344aae450144545e
yoezequiel <ezequielcasiano15@gmail.com>
2026-01-04 20:08:05 -0300

    Add Tailwind CSS configuration and styles
    - Created tailwind.css file with base, components, and utilities imports.
    - Added tailwind.config.mjs for Tailwind CSS configuration with content paths.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
 export default defineConfig({
     site: "https://POS-yoezequiel.vercel.app", // Cambiar a la URL de producción cuando se despliegue
-    integrations: [react()],
+    integrations: [react(), tailwind()],
     server: {
         port: 4321,
     },
